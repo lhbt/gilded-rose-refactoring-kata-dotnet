0e421a7312209e409a1ba518bd2925d6c4696d3c
Laurent Humbert <laurent@accommodationforstudents.com>
2024-02-14 17:25:36 +0000

    refactor: rename variables and setup method in tests for better readability

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -15,7 +15,7 @@ public class GildedRoseTest
         Assert.That(items[0].Name, Is.EqualTo("foo"));
     }
 
-    private Item Setup(string itemName, int startingSellIn, int startingQuality)
+    private Item SetupInventoryAndUpdateItemQuality(string itemName, int startingSellIn, int startingQuality)
     {
         var item = new Item(itemName, startingSellIn, startingQuality);
         var inventory = new GildedRose.GildedRose(new List<Item> { item });
@@ -28,56 +28,56 @@ public class GildedRoseTest
     [Test]
     public void quality_and_sell_in_of_a_normal_item_decrease_when_a_day_passes()
     {
-        var item = Setup("random item", 1, 1);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("random item", 1, 1);
 
-        Assert.That(item.SellIn, Is.EqualTo(0));
-        Assert.That(item.Quality, Is.EqualTo(0));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(0));
+        Assert.That(updatedItem.Quality, Is.EqualTo(0));
     }
 
     [Test]
     public void quality_decreases_twice_as_fast_once_sell_by_date_has_passed()
     {
-        var item = Setup("another random item", 0, 2);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("another random item", 0, 2);
         
-        Assert.That(item.SellIn, Is.EqualTo(-1));
-        Assert.That(item.Quality, Is.EqualTo(0));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(-1));
+        Assert.That(updatedItem.Quality, Is.EqualTo(0));
     }
 
     [TestCase("Aged Brie")]
     [TestCase("Backstage passes to a TAFKAL80ETC concert")]
     public void quality_of_aged_brie_and_backstage_passes_can_never_be_over_50(string itemName)
     {
-        var item = Setup(itemName, 4, 50);
+        var updatedItem = SetupInventoryAndUpdateItemQuality(itemName, 4, 50);
         
-        Assert.That(item.SellIn, Is.EqualTo(3));
-        Assert.That(item.Quality, Is.EqualTo(50));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(3));
+        Assert.That(updatedItem.Quality, Is.EqualTo(50));
     }
 
     [Test]
     public void quality_can_never_be_negative()
     {
-        var item = Setup("quality 0, danger incoming!", 1, 0);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("quality 0, danger incoming!", 1, 0);
         
-        Assert.That(item.SellIn, Is.EqualTo(0));
-        Assert.That(item.Quality, Is.EqualTo(0));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(0));
+        Assert.That(updatedItem.Quality, Is.EqualTo(0));
     }
 
     [Test]
     public void aged_brie_increases_in_quality_as_it_gets_older()
     {
-        var item = Setup("Aged Brie", 1, 0);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("Aged Brie", 1, 0);
         
-        Assert.That(item.SellIn, Is.EqualTo(0));
-        Assert.That(item.Quality, Is.EqualTo(1));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(0));
+        Assert.That(updatedItem.Quality, Is.EqualTo(1));
     }
 
     [Test]
     public void sulfuras_never_decreases_sell_in_or_quality()
     {
-        var item = Setup("Sulfuras, Hand of Ragnaros", 1, 1);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("Sulfuras, Hand of Ragnaros", 1, 1);
         
-        Assert.That(item.SellIn, Is.EqualTo(1));
-        Assert.That(item.Quality, Is.EqualTo(1));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(1));
+        Assert.That(updatedItem.Quality, Is.EqualTo(1));
     }
 
     [TestCase(15, 1, 14, 2)]
@@ -87,9 +87,9 @@ public class GildedRoseTest
     public void backstage_passes_quality_varies_as_days_pass_based_on_sell_in_value
         (int startingSellIn, int startingQuality, int expectedUpdatedSellIn, int expectedUpdatedQuality)
     {
-        var item = Setup("Backstage passes to a TAFKAL80ETC concert", startingSellIn, startingQuality);
+        var updatedItem = SetupInventoryAndUpdateItemQuality("Backstage passes to a TAFKAL80ETC concert", startingSellIn, startingQuality);
         
-        Assert.That(item.SellIn, Is.EqualTo(expectedUpdatedSellIn));
-        Assert.That(item.Quality, Is.EqualTo(expectedUpdatedQuality));
+        Assert.That(updatedItem.SellIn, Is.EqualTo(expectedUpdatedSellIn));
+        Assert.That(updatedItem.Quality, Is.EqualTo(expectedUpdatedQuality));
     }
 }
