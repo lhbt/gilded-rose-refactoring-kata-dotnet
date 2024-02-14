e45c53c14b9846d5e7a8fc5149712560a6ddfefe
Laurent Humbert <laurent@accommodationforstudents.com>
2024-02-14 18:07:27 +0000

    refactor: reorder and simplify backstage pass logic for better readability

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -26,46 +26,36 @@ public class GildedRose
                 IncreaseQuality(item);
                 DecreaseSellIn(item);
 
-                if (item.SellIn < 0)
-                {
-                    IncreaseQuality(item);
-                }
+                if (item.SellIn < 0) IncreaseQuality(item);
 
                 continue;
             }
 
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
+                DecreaseSellIn(item);
+                
                 IncreaseQuality(item);
 
-                if (item.SellIn < 11)
+                if (item.SellIn < 10)
                 {
                     IncreaseQuality(item);
                 }
 
-                if (item.SellIn < 6)
+                if (item.SellIn < 5)
                 {
                     IncreaseQuality(item);
                 }
 
-                DecreaseSellIn(item);
-
-                if (item.SellIn < 0)
-                {
-                    item.Quality = 0;
-                }
+                if (item.SellIn < 0) item.Quality = 0;
 
                 continue;
             }
             
             DecreaseQuality(item);
-
             DecreaseSellIn(item);
 
-            if (item.SellIn < 0)
-            {
-                DecreaseQuality(item);
-            }
+            if (item.SellIn < 0) DecreaseQuality(item);
         }
     }
 
@@ -76,12 +66,11 @@ public class GildedRose
 
     private static void DecreaseQuality(Item item)
     {
-        if (item.Quality > MinQuality)
-            item.Quality -= 1;
+        if (item.Quality > MinQuality) item.Quality -= 1;
     }
 
-    private static void IncreaseQuality(Item item)
+    private static void IncreaseQuality(Item item, int value = 1)
     {
-        if (item.Quality < MaxQuality) item.Quality += 1;
+        if (item.Quality < MaxQuality) item.Quality += value;
     }
 }
